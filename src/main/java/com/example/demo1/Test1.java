29eba7592837b0d91dd54ca480e04a19ba4fd535
Proscanu Cosmin <proscanu.cosmin@gmail.com>
2021-12-18 10:06:12 +0200

    added new logic

--- src/main/java/com/example/demo1/Test1.java
+++ src/main/java/com/example/demo1/Test1.java
@@ -3,5 +3,7 @@ package com.example.demo1;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Test branch 1");
+        int a = 10;
+        int sum = a + 1000;
     }
 }
